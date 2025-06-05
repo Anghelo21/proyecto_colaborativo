e2791ade668c8f65c80d0c58b9bbe02bcfba2c47
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-05 16:12:44 -0500

    Optimización de Ejercicio 1 (secuenciales):Se mejoro la interfaz al usuario y se agrego una validación de persuación al usuario de no emplear numeros negativos

--- Ejercicio 1 (secuenciales).cpp
+++ Ejercicio 1 (secuenciales).cpp
@@ -3,11 +3,37 @@ using namespace std;
 
 int main(){
 	int b, h, P;
-	cout<<"Ingrese la base: ";
+	cout<<">>>=====================================<<<: "<<endl;
+	cout<<"   Calculadora - Perimetro del Recangulo"<<endl;
+	cout<<">>>=====================================<<<: "<<endl<<endl;
+	cout<<"perimetro = 2 * (base + altura)"<<endl<<endl;
+	cout<<"A. Ingresa la base del rectangulo: "<<endl;
 	cin>> b;
-	cout<<"Ingrese la altura: ";
+	while (b <= 0) {
+        cout<<"¡Numero invalido!"<< endl;
+        cout<<"Recuerda ingresar un numero positivo en la base." << endl;
+		cin>> b;
+    }
+	cout<<"B. Ingresa la altura del rectangulo: "<<endl;
 	cin>> h;
+	while (h <= 0) {
+        cout<<"¡Numero invalido!"<<endl;
+		cout<<"Recuerda ingresar un numero positivo en la altura." << endl;
+        cin>> h;
+    }
 	P= 2*(b+h);
-	cout<<"El perímetro de tu rectángulo es: "<<P<<endl;
+	cout<<""<<endl;
+	cout<<"El perimetro de tu rectangulo es: "<<endl<<P<<endl;
+
+	cout<<""<<endl;
+	cout<<"Listo, si te sirve de algo aqui esta tu rectangulo dibujado:"<<endl<<endl;
+
+		for (int i = 0; i < h; i++) {
+        for (int j = 0; j < b; j++) {
+            cout << "+ ";
+        }
+        cout << endl;
+}
 	return 0;
 }
+
