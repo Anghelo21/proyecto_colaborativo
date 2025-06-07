750cde29474775f945cb8e506e0d2b44ad890827
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-06 22:39:32 -0500

    Optimización de Ejercicio 2 (secuenciales): Se añadió una validación con el blucle do while para no permitir números negativos y Se mejoro la interfaz para el usuario con un diseño atractivo.

--- Ejercicio 2 (secuenciales).cpp
+++ Ejercicio 2 (secuenciales).cpp
@@ -3,11 +3,20 @@ using namespace std;
 
 int main(){
 	double s, d, C;
-	cout<<"Ingrese la cantidad de soles: ";
-	cin>> s;
-	cout<<"Ingrese el valor del dolar: ";
-	cin>> d;
-	C= s/d;
-	cout<<"Su cambio es: "<<C<<endl;
+	cout << "*****************************" << endl;
+	cout << "*    CONVERSOR DE MONEDAS   *" << endl;
+	cout << "*     (SOLES -> DOLARES)    *" << endl;
+	cout << "*****************************" << endl<<endl;
+	cout << "Cantidad              Convertido a"<<endl;
+	cout << "1 sol peruano    =    0.27 dolares"<<endl<<endl;
+	do{
+		cout<<"Ingresa tu cantidad de soles:"<<endl<<"S/.";
+		cin>> s;
+		if (s <= 0) {
+		cout <<""<<endl<<"¡CANTIDAD INVALIDA!"<<endl<<"Recuerda ingresar una cantidad positiva por favor."<<endl<<endl;
+		}
+	}while(s <= 0);
+	C= s*0.27;
+	cout<<""<<endl<<"¡Listo!"<<endl<<"Su cambio equivalente a dolares es:"<<endl<<"$"<< C <<endl;
 	return 0;
 }
