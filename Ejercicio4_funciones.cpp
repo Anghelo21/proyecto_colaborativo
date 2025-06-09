8c7616f2f63f19813cad2abdd73d60a342faacce
Josue Adrian Sosa Cruz <jasosac@unjbg.edu.pe>
2025-06-09 09:05:11 -0500

    Optimizacion del ejercicio 4 de funciones: Para obtener el número mayor usando max()

--- Ejercicio4_funciones.cpp
+++ Ejercicio4_funciones.cpp
@@ -1,25 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int Mayor(int a, int b, int c){
-	int M;
-	if(a<b){
-		if(b<c){
-			M=c;
-		}else{
-			M=b;
-		}
-	}else if(a<c){
-		M=c;
-	}else{
-		M=a;
-	}
-	return M;
+
+int Mayor(int a, int b, int c) {
+    return max(a, max(b, c));
 }
-int main(){
-	int n1,n2,n3,Ma;
-	cout<<Ingrese tres numeros<<endl;
-	cin>> n1 >> n2 >> n3;
-	Ma=Mayor(n1,n2,n3);
-	cout<<El numero mayor es: <<Ma<<endl;
-	return 0;
+
+int main() {
+    int n1, n2, n3;
+    cout << "Ingrese tres numeros: " << endl;
+    cin >> n1 >> n2 >> n3;
+
+    int Ma = Mayor(n1, n2, n3);
+    cout << "El numero mayor es: " << Ma << endl;
+
+    return 0;
 }
